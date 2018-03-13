c1f34b3efb6d5786c3db534400f7cb684a00f444
Pavuluri, Venu <Venu.Pavuluri@pse.com>
2018-03-13 09:58:50 -0700

    Merged PR 631: Merge customer-localbranch to master
    Removed TaxId from lookup call
    
    Related work items: #691, #2033

--- src/V1/Models/LookupCustomer.cs
+++ src/V1/Models/LookupCustomer.cs
@@ -3,20 +3,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PSE.Customer.V1.Models
 {
-    public class LookupCustomer: IValidatableObject
+    public class LookupCustomer
     {
         [Required]
         public string NameOnBill { get; set; }
         [Required]
-        public string ContractAccountNumber { get; set; }        
-        public string TaxId { get; set; }
-
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            var results = new List<ValidationResult>();
-            if (string.IsNullOrEmpty(ContractAccountNumber) && string.IsNullOrEmpty(TaxId))
-                results.Add(new ValidationResult("At least one object is null or empty"));
-            return results;
-        }
+        public string ContractAccountNumber { get; set; }
     }
 }
