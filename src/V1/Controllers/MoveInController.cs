87753112f897df9aebd17ccb0dea3f0a40c44f79
Pavuluri, Venu <Venu.Pavuluri@pse.com>
2018-04-04 08:29:43 -0700

    Merged PR 880: Mock endpoints for latepayments
    Mock endpoints for latepayments
    
    Related work items: #3381

---
+++ src/V1/Controllers/MoveInController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PSE.WebAPI.Core.Service;
+
+namespace PSE.Customer.V1.Controllers
+{
+    public class MoveInLatePaymentsResponse
+    {
+        public decimal? FirstIp { get; set; }
+        public bool? EligibleRc { get; set; }
+        public long AccountNo { get; set; }
+        public bool? ReconnectFlag { get; set; }
+        public long? PriorObligationAccount { get; set; }
+        public decimal? DepositAmount { get; set; }
+        public decimal? ReconAmount { get; set; }
+        public decimal? MinPayment { get; set; }
+        public decimal? IncPayment { get; set; }
+        public string AccountType { get; set; }
+        public string ReasonCode { get; set; }
+        public string Reason { get; set; }
+    }
+
+    [ApiVersion("1.0")]
+    [Produces("application/json")]
+    [Route("v{version:apiVersion}/customer/")]
+    public class MoveInController : PSEController
+    {
+        /// <summary>
+        /// Mock Endpoint for Latepayments movein 
+        /// <br/>
+        /// A    "FirstIp" : "286.000", (first installment which is 50% of (B) ie total deposit )
+        ///B    "DepositAmount" : "572.000", (total deposit amount) 
+        ///C    "ReconAmount" : "70.000", (reconnection fees)
+        ///D    "MinPayment" : "356.000", (Item A + B)
+        ///E    "IncPayment" : "320.000",  (Payment which have been made)
+        ///F    "EligibleRc" : "", (If this is ‘X’ it means customer has made thresh-hold payment and s/he is eligible for reconnection)
+        ///If ReconnectFlag eq 'X', it will initiate reconnection
+        /// </summary>
+        /// <param name="reconnectFlag">If ReconnectFlag eq 'X', it will initiate reconnection</param>
+        /// <returns></returns>
+        [HttpGet("latepayment-movein/{contractAccountId}")]
+        [ProducesResponseType(typeof(MoveInLatePaymentsResponse), 200)]
+        public async Task<IActionResult> MoveInLatePayments([FromQuery]bool reconnectFlag)
+        {
+            IActionResult result = Ok(new MoveInLatePaymentsResponse()
+            {
+
+                FirstIp = 286.00m,
+                EligibleRc = true,
+                AccountNo = 200028750178,
+                ReconnectFlag = false,
+                PriorObligationAccount = 200026140646,
+                DepositAmount = 572.00m,
+                ReconAmount = 70.00m,
+                MinPayment = 356,
+                IncPayment = 320.00m,
+                AccountType = "RES",
+                ReasonCode = string.Empty,
+                Reason = string.Empty
+            });
+
+            return result;
+        }
+    }
+}
